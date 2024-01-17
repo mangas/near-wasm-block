6ca690d9e28e5e70fe355f7adc69b94233f2b45e
Filipe Azevedo <filipe@azevedo.io>
2024-01-17 15:37:49 +0000

    update to latest cli

--- src/mapping.ts
+++ src/mapping.ts
@@ -15,7 +15,7 @@ export function handleBlock(blockBytes: Uint8Array): void {
 
   let event = new BlockEvent("0x".concat(header.hash.bytes.toString()));
   event.number = BigInt.fromI32(header.height as i32);
-  // event.hash =Bytes.fromUint8Array(Uint8Array.wrap( header.hash.encode()));
+  // event.hash = Bytes.fromUint8Array(Uint8Array.wrap(header.hash.encode()));
   event.timestampNanosec = BigInt.fromU64(header.timestamp_nanosec);
   // event.gasPrice = BigInt.fromU64(Bytes.fromUint8Array(Uint8Array.wrap(header.gas_price.encode())).toU64());
 
